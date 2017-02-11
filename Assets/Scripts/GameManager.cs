1925f45ebc226a25bf1d76a3426257dc09f5ede9
SvenFrankson <sven.frankson@gmail.com>
2017-02-11 02:16:08 +0100

    Add MainMenu GUI.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -27,6 +27,8 @@ public class GameManager : MonoBehaviour {
 	public List<Level> Levels;
 	private Level LevelInstance;
 
+    public GUISkin Skin;
+
 	public void Start() {
 		this.PlayerSpaceshipInstance = Instantiate<Spaceship> (this.PlayerSpaceshipPrefab);
 	}
@@ -59,12 +61,58 @@ public class GameManager : MonoBehaviour {
 	}
 
 	public void OnGUI() {
+        GUI.skin = this.Skin;
+        GUILabelResponsive(20, 50, 20, 50, "Appolo");
 		if (!GameManager.gameOn) {
-			foreach (Level level in this.Levels) {
-				if (GUILayout.Button (level.name)) {
-					this.LoadLevel (level);
-				}
-			}
+            for (int i = 0; i < this.Levels.Count; i++)
+            {
+                int x = i % 4;
+                int y = i / 4;
+                if (GUIButtonResponsive(y * 20 + 60, (x + 1) * 20, 15, 15, this.Levels[i].name))
+                {
+                    this.LoadLevel(this.Levels[i]);
+                }
+            }
 		}
 	}
+
+    public void GUILabelResponsive(int top, int right, int height, int width, string text)
+    {
+        GUILabelResponsive(top / 100f, right / 100f, height / 100f, width / 100f, text);
+    }
+
+    public void GUILabelResponsive(float top, float right, float height, float width, string text)
+    {
+        int h = Screen.height;
+        int w = Screen.width;
+
+        int pHeight = Mathf.RoundToInt(height * h);
+        int pWidth = Mathf.RoundToInt(width * w);
+        int pTop = Mathf.RoundToInt(top * h) - pHeight / 2;
+        int pRight = Mathf.RoundToInt(right * w) - pWidth / 2;
+
+        Rect position = new Rect(pRight, pTop, pWidth, pHeight);
+
+        GUI.Label(position, text, "MainTitle");
+    }
+
+    public bool GUIButtonResponsive(int top, int right, int height, int width, string text)
+    {
+        return GUIButtonResponsive(top / 100f, right / 100f, height / 100f, width / 100f, text);
+    }
+
+    public bool GUIButtonResponsive(float top, float right, float height, float width, string text)
+    {
+        int h = Screen.height;
+        int w = Screen.width;
+
+        int pHeight = Mathf.RoundToInt(height * h);
+        int pWidth = Mathf.RoundToInt(width * w);
+        int pTop = Mathf.RoundToInt(top * h) - pHeight / 2;
+        int pRight = Mathf.RoundToInt(right * w) - pWidth / 2;
+
+        Rect position = new Rect(pRight, pTop, pWidth, pHeight);
+
+        return GUI.Button(position, text, "MainLevel");
+    }
 }
